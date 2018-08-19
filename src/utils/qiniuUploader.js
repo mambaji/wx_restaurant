192d2ce18b95386ded123c34926ae587d37d1e07
manbasji <276066021@qq.com>
2018-08-19 17:58:51 +0800

    小程序优化

--- src/utils/qiniuUploader.js
+++ src/utils/qiniuUploader.js
@@ -164,7 +164,7 @@ function uploadURLFromRegionCode(code) {
     switch(code) {
         case 'ECN': uploadURL = 'https://up.qbox.me'; break;
         case 'NCN': uploadURL = 'https://up-z1.qbox.me'; break;
-        case 'SCN': uploadURL = 'https://up-z2.qbox.me'; break;
+        case 'SCN': uploadURL = 'https://upload-z2.qiniup.com'; break;
         case 'NA': uploadURL = 'https://up-na0.qbox.me'; break;
         case 'ASG': uploadURL = 'https://up-as0.qbox.me'; break;
         default: console.error('please make the region is with one of [ECN, SCN, NCN, NA, ASG]');
